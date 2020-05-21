2fae9e8086bb8f8473eb6b066894d39cdae20505
tomfrankkirk <tomfrankkirk@gmail.com>
2020-05-21 16:30:02 +0100

    jacobian determiant function

--- regtricks/fnirt_coefficients.py
+++ regtricks/fnirt_coefficients.py
@@ -99,8 +99,8 @@ class NonLinearProduct(object):
             pmat = itertools.repeat(postmat.to_fsl(self.warp2.ref_spc, ref))
 
         if len(self.midmat) > 1: 
-            same2 = all([ np.allclose(midmat.src2ref_world[0], m) 
-                          for m in midmat.src2ref_world ])
+            same2 = all([ np.allclose(self.midmat.src2ref_world[0], m) 
+                          for m in self.midmat.src2ref_world ])
             if same2: 
                 mmat = itertools.repeat(self.midmat.transforms[0].to_fsl(self.warp1.ref_spc, self.warp2.src_spc))
             else: 
@@ -152,3 +152,54 @@ def get_field(coeff1, ref, coeff2=None, mid=None, post=None):
         subprocess.run(cmd, shell=True)  
         field = nibabel.load(field).get_data().reshape(-1,3)
     return field 
+
+
+def det_jacobian(vec_field, vox_size):
+    """
+    Calculate determinant of Jacobian for vector field, with homogenous
+    spacing along each axis. Second order central differences are used 
+    to estimate partial derivatives. 
+
+    Args: 
+        vec_field (np.ndarray): sized XYZ3, where the last dimension 
+            corresponds to displacements along the x,y,z axis respectively
+        vox_size (np.ndarray): array sized 3, step size along each spatial axis
+
+    Returns: 
+        (np.ndarray), sized XYZ, values of the determinant of the Jacobian
+            matrix evaluated at each point within the array 
+
+    """
+
+    if not ((len(vec_field.shape) == 4) and (vec_field.shape[3] == 3)):
+        raise ValueError("vec_field should be a 4D array with size 3 in "
+            "the last dimension")
+
+    if not len(vox_size) == 3: 
+        raise ValueError("vox_size should be a 3-vector of dimensions") 
+
+    # Calculate partial derivatives in each direction. Note that each of these
+    # returns an array of size (X,Y,Z,3), arranged d/dx, d/dy, d/dz in the last
+    # dimension. So dfx is a stack of arrays df(i)/dx, df(i)/dy, df(i)/dz. We
+    # need to calculate the derivative with respect to each direction because
+    # f is a vector field, ie, it contains i,j,k components, each of which are
+    # independent functions of x,y,z
+    dfi = np.gradient(vec_field, vox_size[0], axis=0)
+    dfj = np.gradient(vec_field, vox_size[1], axis=1)
+    dfk = np.gradient(vec_field, vox_size[2], axis=2)
+
+    # Construct an array of Jacobian matrices, sized (3,3,X,Y,Z) (ie, each 
+    # point in space get its own 3x3 Jacobian matrix). The elements need
+    # to be arranged df(i)/dx --- df(i)/dz 
+    #                   |      |     |
+    #                df(k)/dx --- df(k)/dz
+    jacobian = np.array([[dfi[...,0], dfi[...,1], dfi[...,2]],
+                         [dfj[...,0], dfj[...,1], dfj[...,2]],
+                         [dfk[...,0], dfk[...,1], dfk[...,2]]
+                        ])
+
+    # Reshape into a single stack of (N,3,3) arrays and calculate the det
+    # Return an array of scalars sized (XYZ) again 
+    jacobian = np.moveaxis(jacobian.reshape(3,3,-1), 2, 0)
+    jdet = np.linalg.det(jacobian)
+    return jdet.reshape(vec_field.shape[:3])
